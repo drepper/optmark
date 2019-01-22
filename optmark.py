642bb118311780f49670ce3bbbfd0c0d0fa7b8a4
Ulrich Drepper <drepper@gmail.com>
2019-01-22 23:56:38 +0100

    Show pass information.  Read all pass data.  Support scope type.

--- optmark.py
+++ optmark.py
@@ -10,6 +10,8 @@ def read_passes(tree):
     result = {}
     for e in tree:
         result[e['id']] = e['name']
+        if 'children' in e:
+            result = { **result, **read_passes(e['children']) }
     return result
 
 
@@ -33,7 +35,7 @@ def loc_compare(rec1, rec2):
     return 0
 
 
-colors = { 'success': '\x1b[1;37;42m', 'failure': '\x1b[1;41m', 'note': '\x1b[1;37;44m' }
+colors = { 'success': '\x1b[1;37;42m', 'failure': '\x1b[1;41m', 'note': '\x1b[1;37;44m', 'scope': '\x1b[45m' }
 coloroff = '\x1b[0m'
 
 inlwhat = { 'Inlining': 1, 'Inlined': 1, 'will not early inline:': 3, 'not inlinable:': 3, 'Considering inline candidate': 1 }
@@ -54,6 +56,8 @@ def handle(root, tree, relative, interesting):
                     linedata = { 'line': l['line'], 'column': l['column'], 'kind': r['kind'], 'what': r['message'][0].strip() }
                     if linedata['what'] in inlwhat:
                         linedata['inlfunction'] = r['message'][inlwhat[linedata['what']]]['symtab_node']
+                    if 'pass' in r and r['pass'] in passes:
+                        linedata['pass'] = passes[r['pass']]
                     files[fn].append(linedata)
         elif 'impl_location' in r:
             pass
@@ -77,6 +81,8 @@ def handle(root, tree, relative, interesting):
                         s = "Inlining " + loc['inlfunction']
                     else:
                         s = loc['what']
+                    if 'pass' in loc:
+                        s += ' ({} pass)'.format(loc['pass'])
                     doprint = True
                     if loc['line'] != lastline['line'] or loc['column'] != lastline['column']:
                         lastline = { 'line': loc['line'], 'column': loc['column'], 'what': [ ] }
@@ -87,7 +93,7 @@ def handle(root, tree, relative, interesting):
                                 break
                     if doprint:
                         lastline['what'].append(s)
-                        print('{}      {}^ {}{}'.format(colors[loc['kind']], " "*(loc['column']-1), s, coloroff))
+                        print('{}{}^ {}{}'.format(colors[loc['kind']], " "*(loc['column']+5), s, coloroff))
                     try:
                         loc = next(itloc) if itloc else None
                     except StopIteration:
